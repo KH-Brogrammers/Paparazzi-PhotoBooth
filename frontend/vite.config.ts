6898fdc9203ca335e51ce500b3d028a38fe69186
skumar38017 <skumar38017@gmail.com>
2026-01-28 09:24:56 +0530

    commit out

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,21 +8,21 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react(), tailwindcss()],
-    server: {
-      host: true,       
-      port: 5173,
-      strictPort: true,
-      hmr: {
-        host: env.VITE_API_BACKEND_URL 
-                ? env.VITE_API_BACKEND_URL.replace(/^https?:\/\//, '') 
-                : '13.233.215.148',
+    // server: {
+    //   host: true,       
+    //   port: 5173,
+    //   strictPort: true,
+    //   hmr: {
+    //     host: env.VITE_API_BACKEND_URL 
+    //             ? env.VITE_API_BACKEND_URL.replace(/^https?:\/\//, '') 
+    //             : '13.233.215.148',
         
-        // Use 'ws' for IP/HTTP or 'wss' for Domain/HTTPS
-        protocol: env.VITE_API_BACKEND_URL?.startsWith('https') ? 'wss' : 'ws',
+    //     // Use 'ws' for IP/HTTP or 'wss' for Domain/HTTPS
+    //     protocol: env.VITE_API_BACKEND_URL?.startsWith('https') ? 'wss' : 'ws',
         
-        // If hitting through Nginx (port 80), the browser thinks the port is 80
-        clientPort: env.VITE_API_BACKEND_URL?.startsWith('https') ? 443 : 80,
-      },
-    },
+    //     // If hitting through Nginx (port 80), the browser thinks the port is 80
+    //     clientPort: env.VITE_API_BACKEND_URL?.startsWith('https') ? 443 : 80,
+    //   },
+    // },
   };
 });
