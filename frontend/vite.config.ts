ae4bfbde4fb76fa08c11420ca981817f80e88797
skumar38017 <skumar38017@gmail.com>
2026-01-29 00:40:03 +0530

    cors

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
       host: true,       
       port: 5173,
       strictPort: true,
-      allowedHosts: ['*'],
+      allowedHosts: ['magictap.app'],
       hmr: {
         host: env.VITE_API_BACKEND_URL 
                 ? env.VITE_API_BACKEND_URL.replace(/^https?:\/\//, '') 
